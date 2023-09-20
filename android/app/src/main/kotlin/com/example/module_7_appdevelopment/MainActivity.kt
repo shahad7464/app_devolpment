fb09b590f97be4c9a872159245675cffcad48a7d
Joynal Abedin Shahad <bjse4ever7464@gmail.com>
2023-09-20 23:02:47 +0600

    CuperTino Widget pushed

---
+++ android/app/src/main/kotlin/com/example/module_7_appdevelopment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_7_appdevelopment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Icon(CupertinoIcons.home),
+        middle: Text('Home'),
+        trailing: CupertinoSwitch(
+          onChanged: (bool onChange) {
+
+          }, value: true,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            CupertinoButton.filled(child: Text('sfd'), onPressed: () {}),
+            CupertinoTextField(),
+            CupertinoTabBar(items: [
+              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
+              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
+              BottomNavigationBarItem(label: 'sdfd', icon: Icon(CupertinoIcons.home)),
+            ])
+          ],
+        ),
+      ),
+    );
+  }
+}
