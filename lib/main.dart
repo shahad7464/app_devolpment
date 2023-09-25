a814efc507b2a16cd6664af60ecd3422ee4d0e1f
Joynal Abedin Shahad <bjse4ever7464@gmail.com>
2023-09-25 16:29:22 +0600

    not complete yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,197 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // give me some widget
-  runApp(MyApp());
+main() {
+  runApp(
+    const MyApp(),
+  );
 }
 
-// Hot reload => ctl + s
-// Hot restart => ctl + shift + \
-
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
-      title: 'Profile',
+    return const MaterialApp(
+      home: ProductList(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class ProductList extends StatefulWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  State<ProductList> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<ProductList> {
+  int counter = 0;
+  void incrementCounter() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+        title: const Text('Product List'),
         centerTitle: true,
-        title: Text('Profile'),
-        leading: Icon(
-          Icons.adb_rounded,
-          color: Colors.amber,
-          size: 32,
-        ),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('To register, tap on '),
-            ],
-          ),
-          RichText(
-            text: TextSpan(
-              style: TextStyle(fontSize: 20, color: Colors.purple),
+          Divider(height: 15,),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextSpan(
-                    text: 'Hello ', style: TextStyle(color: Colors.purple)),
-                TextSpan(text: 'Name ', style: TextStyle(color: Colors.amber)),
-                TextSpan(
-                    text: 'Is skflds ',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600)),
+                Text('Product No:1'),
+                Text('USD 10.00') ,
               ],
             ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(onPressed: null, child: Text('Buy Now')),
+
+              ],
+            )
           ),
+          Divider(height: 20),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product No:2'),
+                Text('USD 20.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 20),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product No:3'),
+                Text('USD 25.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product No: 4'),
+                Text('USD 30.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:5'),
+                Text('USD 35.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:6'),
+                Text('USD 40.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:7'),
+                Text('USD 45.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:7'),
+                Text('USD 45.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:8'),
+                Text('USD 50.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10,),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:9'),
+                Text('USD 55.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10,),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:10'),
+                Text('USD 60.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10,),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:11'),
+                Text('USD 65.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          ),
+          Divider(height: 10,),
+          ListTile(
+            leading: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product no:12'),
+                Text('USD 70.00'),
+              ],
+            ),
+            trailing:ElevatedButton(onPressed: null, child: Text('Buy Now')),
+          )
+
         ],
       ),
-    );
+
+     );
+
   }
 }
